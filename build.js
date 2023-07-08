bfabe28e98d7a7506d54d3e68d3ceff9de025124
utftufutukgyftryidytftuv <overby999@gmail.com>
2023-07-08 13:28:04 +0300

    08.07.23 13:28

--- build.js
+++ build.js
@@ -1,71 +1,3 @@
-// import {readFileSync, writeFileSync} from 'fs';
-// import {build, defineConfig} from 'vite';
-
-// class PackageConfig {
-//   constructor({name, external}) {
-//     this.name = name;
-//     this.external = external;
-//   }
-
-//   get dir() {
-//     return `./packages/${this.name}`;
-//   }
-
-//   get entry() {
-//     return `${this.dir}/${this.name}.js`;
-//   }
-// }
-
-// const packages = [
-//   new PackageConfig({
-//     name: 'controlled-promise',
-//     external: [],
-//   }),
-//   new PackageConfig({
-//     name: 'ee',
-//     external: [],
-//   }),
-//   new PackageConfig({
-//     name: 'float',
-//     external: [],
-//   }),
-//   new PackageConfig({
-//     name: 'delayed-calls',
-//     external: [],
-//   }),
-//   new PackageConfig({
-//     name: 'wait-time',
-//     external: [],
-//   }),
-// ];
-
-// const packageJson = JSON.parse(readFileSync('./package.json').toString());
-// packageJson.exports = packages.reduce((store, {dir, name}) => {
-//   store[`./${name}`] = {
-//     import: `${dir}/dist/${name}.js`,
-//     require: `${dir}/dist/${name}.cjs`,
-//   };
-//   return store;
-// }, {});
-// writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
-// for (const {name, entry, dir, external} of packages) {
-//   await build({
-//     build: {
-//       target: 'esnext',
-//       outDir: `${dir}/dist`,
-//       name,
-//       lib: {
-//         fileName: name,
-//         entry: [entry],
-//         formats: ['es', 'cjs'],
-//       },
-//       rollupOptions: {
-//         external: external,
-//       },
-//     },
-//   });
-// }
-
 import {build} from 'vite';
 import dts from 'vite-plugin-dts';
 
@@ -75,11 +7,15 @@ await build({
     outDir: `./dist`,
     lib: {
       entry: ['./utftu.ts'],
-      formats: ['es', 'cjs'],
+      formats: ['es'],
     },
     rollupOptions: {
       external: [],
     },
   },
-  plugins: [dts()],
+  plugins: [
+    dts({
+      outDir: './dist/types',
+    }),
+  ],
 });
