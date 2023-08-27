33a1450bf71b146ce4a649f8224bb61a0d8c73b8
utftufutukgyftryidytftuv <overby999@gmail.com>
2023-08-27 21:28:10 +0300

    27.08.23 21:28

--- packages/optimized/optimized.ts
+++ packages/optimized/optimized.ts
@@ -1,20 +1,62 @@
 export function createOptimizedValue<TValue = any>(
   func: () => TValue,
   prod: boolean,
-  name: string
+  name: string = '_createOptimizedValue'
 ): TValue {
   let value: TValue;
 
   if (prod === false) {
     if (name in globalThis) {
-      value = globalThis[name];
+      value = (globalThis as any)[name];
     } else {
-      globalThis[name] = func();
-      value = globalThis[name];
+      (globalThis as any)[name] = func();
+      value = (globalThis as any)[name];
     }
   } else {
     value = func();
   }
 
   return value;
+}
+
+export function createOptimizedCachedValue<TCb extends (...args: any[]) => any>(
+  func: TCb,
+  prod: boolean,
+  name: string = '_createOptimizedCachedValue'
+) {
+  let state: Record<string, ReturnType<TCb>>;
+
+  if (prod === false && typeof globalThis !== 'undefined') {
+    if (name in globalThis) {
+      state = (globalThis as any)[name];
+    } else {
+      state = (globalThis as any)[name] = {};
+    }
+  } else {
+    state = {};
+  }
+
+  if (
+    prod === false &&
+    typeof globalThis !== 'undefined' &&
+    name in globalThis
+  ) {
+    state = (globalThis as any)[name];
+  } else {
+    state = {};
+  }
+
+  return (key: string = 'default') => {
+    const handler = ((...args: Parameters<TCb>) => {
+      if (key in state) {
+        return state[key];
+      }
+
+      const result = func(...args);
+      state[key] = result;
+      return result;
+    }) as typeof func;
+
+    return handler;
+  };
 }
